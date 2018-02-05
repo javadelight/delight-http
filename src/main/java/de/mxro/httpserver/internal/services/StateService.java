7f867cb4303eab0e179c3fa3adaa5e705b9223f2
Max Rohde <mxro@nowhere.com>
2018-02-05 11:28:00 +1100

    Allow requesting multiple states in one request.

--- src/main/java/de/mxro/httpserver/internal/services/StateService.java
+++ src/main/java/de/mxro/httpserver/internal/services/StateService.java
@@ -1,19 +1,23 @@
 package de.mxro.httpserver.internal.services;
 
+import java.util.List;
+
 import de.mxro.httpserver.HttpService;
 import de.mxro.httpserver.Request;
 import de.mxro.httpserver.Response;
 import delight.async.callbacks.SimpleCallback;
 import delight.functional.Closure;
 import delight.functional.SuccessFail;
+import delight.json.JSON;
+import delight.json.JSONArray;
 import delight.state.StateProvider;
 import delight.state.StateRegistry;
 
 public class StateService implements HttpService {
-	
+
 	private final String rootPath;
 	private final StateRegistry registry;
-	
+
 	@Override
 	public void stop(SimpleCallback callback) {
 		callback.onSuccess();
@@ -27,26 +31,41 @@ public class StateService implements HttpService {
 	@Override
 	public void process(Request request, Response response, Closure<SuccessFail> callback) {
 		String requestUri = request.getRequestUri();
-		
+
 		assert requestUri.startsWith(rootPath);
-		
+
 		String path = requestUri.substring(rootPath.length());
-		
-		StateProvider provider = registry.getProvider(path);
-		
-		if (provider == null) {
-			response.setContent("Resource ["+path+"] does not exist or has not been initialized.");
+
+		List<StateProvider> providers = registry.getProviders(path);
+
+		if (providers.size() == 0) {
+			response.setContent("Resource [" + path + "] does not exist or has not been initialized.");
 			response.setResponseCode(404);
 			callback.apply(SuccessFail.success());
 			return;
 		}
+
+		if (providers.size() == 1) {
+
+			response.setContent(providers.get(0).get().render());
+			response.setMimeType("text/json");
+			response.setResponseCode(200);
+
+			callback.apply(SuccessFail.success());
+			return;
+		}
 		
-		response.setContent(provider.get().render());
+		JSONArray ar = JSON.createArray();
+
+		for (StateProvider provider: providers) {
+			ar.push(provider.get());
+		}
+		
+		response.setContent(ar.render());
 		response.setMimeType("text/json");
 		response.setResponseCode(200);
-		
+
 		callback.apply(SuccessFail.success());
-		
 		
 	}
 
@@ -55,7 +74,5 @@ public class StateService implements HttpService {
 		this.rootPath = rootPath;
 		this.registry = registry;
 	}
-	
-	
 
 }
